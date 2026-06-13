43ec8c9f82c78baf68f58c2742b13f5d63d1a189
Pierre-Alexandre Lee <pierrealex.ly@gmail.com>
2026-06-13 09:48:45 +0200

    Initial Commit

---
+++ Nulla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nulla
+//
+//  Created by Pierre-Alexandre Lee on 13.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nulla/NullaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NullaApp.swift
+//  Nulla
+//
+//  Created by Pierre-Alexandre Lee on 13.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct NullaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
